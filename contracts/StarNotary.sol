3762ed6c9abb7723dff64a4631be3382ce537fdc
Davi Paranaguá Vital <daviparanagua@yahoo.com.br>
2021-03-25 07:53:52 -0300

    Token name

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -1,10 +1,13 @@
 pragma solidity >=0.4.24;
 
 //Importing openzeppelin-solidity ERC-721 implemented Standard
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 // StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
-contract StarNotary is ERC721 {
+contract AwesomeStar is ERC721Full {
+
+    constructor() ERC721Full("AwesomeStar", "AWS") public {
+    }
 
     // Star data
     struct Star {
